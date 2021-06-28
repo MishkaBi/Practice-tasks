af5917d9b7bda370e25cd4346eaf01031ce52aa3
Misha Biryuk <vovann7093@gmail.com>
2021-06-28 16:24:16 +0300

    first commit

---
+++ IO practice/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace IO_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter name of person who has a birthday soon");
+            string name = Console.ReadLine();
+
+
+            Console.WriteLine("Enter birthday");
+            DateTime now = DateTime.Now;
+            string birthday1 = Console.ReadLine();
+            var birthday = DateTime.Parse(birthday1);
+
+            TimeSpan age = now - birthday;
+
+            Console.WriteLine(age.TotalSeconds);
+
+
+            Console.WriteLine("Enter a present");
+            string present = Console.ReadLine();
+
+            Console.WriteLine("Enter a giver");
+            string nameOfGiver = Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine($"{name} has a birthday soon!");
+
+            Console.WriteLine($" Current age: {age}  \n Birthday: {birthday}  \n Present: {present} \n Giver: {nameOfGiver} ");
+        }
+    }
+}
