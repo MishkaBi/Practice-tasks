2b6f996ba21f8500d1a8403d451334e4a2b202c4
Misha Biryuk <vovann7093@gmail.com>
2021-07-03 21:53:39 +0300

    new task

---
+++ Conditional expession task/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Conditional_expession_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"The number {number} is even.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is odd.");
+            }
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Enter year");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year % 4 == 0)
+            {
+                Console.WriteLine($"The year {year} is a leap year");
+            }
+            else
+            {
+                Console.WriteLine($"The year {year} isn't a leap year.");
+            }
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Enter three numbers");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+              if (num1 > num3)
+                {
+                    Console.WriteLine("The 1st Number is the greatest among three.");
+                }
+            }
+
+            if (num2 > num1)
+            {
+                if (num2 > num3)
+                {
+                    Console.WriteLine("The 2nd Number is the greatest among three.");
+                }
+            }
+
+            if (num3 > num1)
+            {
+                if (num3 > num2)
+                {
+                    Console.WriteLine("The 3rd Number is the greatest among three.");
+                }
+            }
+
+        }
+    }
+}
--- Pizza order/Program.cs
+++ Pizza order/Program.cs
@@ -8,7 +8,7 @@ namespace Pizza_order
         {
             Console.WriteLine("Вибер1ть з якого т1ста буде складатись п1ца\n1. Бездр1жджове\n2. Др1жджове\n3. Листкове\n4. Чорне");
 
-            int num1 = Convert.ToInt32(Console.ReadLine());
+            int dough = Convert.ToInt32(Console.ReadLine());
 
 
             Console.Clear();
@@ -16,7 +16,7 @@ namespace Pizza_order
 
             Console.WriteLine("Вибер1ть яка начинка буде у п1ц1\n1. Ковбаса\n2. Курка\n3. Бекон\n4. Сир");
 
-            int num2 = Convert.ToInt32(Console.ReadLine());
+            int filling = Convert.ToInt32(Console.ReadLine());
 
 
             Console.Clear();
@@ -24,7 +24,7 @@ namespace Pizza_order
 
             Console.WriteLine("Вибер1ть який соус буде у п1ц1\n1. Кетчуп\n2. Майонез\n3. Тартар\n4. Сирний");
 
-            int num3 = Convert.ToInt32(Console.ReadLine());
+            int sauce = Convert.ToInt32(Console.ReadLine());
 
 
             Console.Clear();
@@ -32,7 +32,7 @@ namespace Pizza_order
 
             Console.WriteLine("Вибер1ть як1 додатков1 1нгред1єнти будуть у п1ц1\n1. Оливки\n2. Пом1дори\n3. Ананас\n4. Креветки");
 
-            int num4 = Convert.ToInt32(Console.ReadLine());
+            int additional = Convert.ToInt32(Console.ReadLine());
 
 
             Console.Clear();
@@ -41,78 +41,109 @@ namespace Pizza_order
             Console.WriteLine($"Ваша п1ца");
 
 
-            if (num1 == 1)
+            if (dough == 1)
             {
                 Console.WriteLine("Т1сто: бездр1жджове");
             }
-            if (num1 == 2)
+            if (dough == 2)
             {
                 Console.WriteLine("Т1сто: др1жджове");
             }
-            if (num1 == 3)
+            if (dough == 3)
             {
                 Console.WriteLine("Т1сто: листкове");
             }
-            if (num1 == 4)
+            if (dough == 4)
             {
                 Console.WriteLine("Т1сто: чорне");
             }
+            if (dough > 4)
+            {
+                Console.WriteLine("Тісто: не вибране");
+            }
+            if (dough < 1)
+            {
+                Console.WriteLine("Тісто: не вибране");
+            }
 
 
-
-            if (num2 == 1)
+            if (filling == 1)
             {
                 Console.WriteLine("Начинка: ковбаса");
             }
-            if (num2 == 2)
+            if (filling == 2)
             {
                 Console.WriteLine("Начинка: курка");
             }
-            if (num2 == 3)
+            if (filling == 3)
             {
                 Console.WriteLine("Начинка: бекон");
             }
-            if (num2 == 4)
+            if (filling == 4)
             {
                 Console.WriteLine("Начинка: сир");
             }
+            if (filling > 4)
+            {
+                Console.WriteLine("Начинка: не вибрана");
+            }
+            if (filling < 1)
+            {
+                Console.WriteLine("Начинка: не вибрана");
+            }
 
 
 
-            if (num3 == 1)
+            if (sauce == 1)
             {
                 Console.WriteLine("Соус: кетчуп");
             }
-            if (num3 == 2)
+            if (sauce == 2)
             {
                 Console.WriteLine("Соус: майонез");
             }
-            if (num3 == 3)
+            if (sauce == 3)
             {
                 Console.WriteLine("Соус: тартар");
             }
-            if (num3 == 4)
+            if (sauce == 4)
             {
                 Console.WriteLine("Соус: сирний");
             }
+            if (sauce > 4)
+            {
+                Console.WriteLine("Соус: не вибраний");
+            }
+            if (sauce < 1)
+            {
+                Console.WriteLine("Соус: не вибраний");
+            }
 
 
 
-            if (num4 == 1)
+            if (additional == 1)
             {
                 Console.WriteLine("Додатков1 1нгред1єнти: оливки");
             }
-            if (num4 == 2)
+            if (additional == 2)
             {
                 Console.WriteLine("Додатков1 1нгред1єнти: пом1дори");
             }
-            if (num4 == 3)
+            if (additional == 3)
             {
                 Console.WriteLine("Додатков1 1нгред1єнти: ананас");
             }
-            if (num4 == 4)
+            if (additional == 4)
             {
                 Console.WriteLine("Додатков1 1нгред1єнти: креветки");
+            }
+            if (additional > 4)
+            {
+                Console.WriteLine("Додатков1 1нгред1єнти: не вибран1");
+            }
+            if (additional < 1)
+            {
+                Console.WriteLine("Додатков1 1нгред1єнти: не вибран1");
             }
         }
     }
