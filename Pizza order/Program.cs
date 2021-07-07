7dc1a9a1b0c9d68c7839c15bb3db96fe6e0d73c1
Misha Biryuk <vovann7093@gmail.com>
2021-07-07 15:21:53 +0300

    fix mistake

--- Pizza order/Program.cs
+++ Pizza order/Program.cs
@@ -7,143 +7,114 @@ namespace Pizza_order
         static void Main(string[] args)
         {
             Console.WriteLine("Вибер1ть з якого т1ста буде складатись п1ца\n1. Бездр1жджове\n2. Др1жджове\n3. Листкове\n4. Чорне");
-
-            int dough = Convert.ToInt32(Console.ReadLine());
-
+            string dough = Console.ReadLine();
 
             Console.Clear();
-
 
             Console.WriteLine("Вибер1ть яка начинка буде у п1ц1\n1. Ковбаса\n2. Курка\n3. Бекон\n4. Сир");
-
-            int filling = Convert.ToInt32(Console.ReadLine());
-
+            string filling = Console.ReadLine();
 
             Console.Clear();
-
 
             Console.WriteLine("Вибер1ть який соус буде у п1ц1\n1. Кетчуп\n2. Майонез\n3. Тартар\n4. Сирний");
-
-            int sauce = Convert.ToInt32(Console.ReadLine());
-
+            string sauce = Console.ReadLine();
 
             Console.Clear();
-
 
             Console.WriteLine("Вибер1ть як1 додатков1 1нгред1єнти будуть у п1ц1\n1. Оливки\n2. Пом1дори\n3. Ананас\n4. Креветки");
-
-            int additional = Convert.ToInt32(Console.ReadLine());
-
+            string additional = Console.ReadLine();
 
             Console.Clear();
-
 
             Console.WriteLine($"Ваша п1ца");
 
 
-            if (dough == 1)
+            if (dough == "1")
             {
                 Console.WriteLine("Т1сто: бездр1жджове");
             }
-            if (dough == 2)
+            else if (dough == "2")
             {
                 Console.WriteLine("Т1сто: др1жджове");
             }
-            if (dough == 3)
+            else if (dough == "3")
             {
                 Console.WriteLine("Т1сто: листкове");
             }
-            if (dough == 4)
+            else if (dough == "4")
             {
                 Console.WriteLine("Т1сто: чорне");
             }
-            if (dough > 4)
+            else
             {
-                Console.WriteLine("Тісто: не вибране");
-            }
-            if (dough < 1)
-            {
-                Console.WriteLine("Тісто: не вибране");
+                Console.WriteLine("Т1сто: не вибране, повтор1ть спробу");
             }
 
 
-            if (filling == 1)
+
+            if (filling == "1")
             {
                 Console.WriteLine("Начинка: ковбаса");
             }
-            if (filling == 2)
+            else if (filling == "2")
             {
                 Console.WriteLine("Начинка: курка");
             }
-            if (filling == 3)
+            else if (filling == "3")
             {
                 Console.WriteLine("Начинка: бекон");
             }
-            if (filling == 4)
+            else if (filling == "4")
             {
                 Console.WriteLine("Начинка: сир");
             }
-            if (filling > 4)
+            else
             {
-                Console.WriteLine("Начинка: не вибрана");
-            }
-            if (filling < 1)
-            {
-                Console.WriteLine("Начинка: не вибрана");
+                Console.WriteLine("Начинка: не вибрана, повтор1ть спробу");
             }
 
 
-
-            if (sauce == 1)
+            if (sauce == "1")
             {
                 Console.WriteLine("Соус: кетчуп");
             }
-            if (sauce == 2)
+            else if (sauce == "2")
             {
                 Console.WriteLine("Соус: майонез");
             }
-            if (sauce == 3)
+            else if (sauce == "3")
             {
                 Console.WriteLine("Соус: тартар");
             }
-            if (sauce == 4)
+            else if (sauce == "4")
             {
                 Console.WriteLine("Соус: сирний");
             }
-            if (sauce > 4)
+            else
             {
-                Console.WriteLine("Соус: не вибраний");
-            }
-            if (sauce < 1)
-            {
-                Console.WriteLine("Соус: не вибраний");
+                Console.WriteLine("Соус: не вибраний, повтор1ть спробу");
             }
 
 
-
-            if (additional == 1)
+            if (additional == "1")
             {
                 Console.WriteLine("Додатков1 1нгред1єнти: оливки");
             }
-            if (additional == 2)
+            else if (additional == "2")
             {
                 Console.WriteLine("Додатков1 1нгред1єнти: пом1дори");
             }
-            if (additional == 3)
+            else if (additional == "3")
             {
                 Console.WriteLine("Додатков1 1нгред1єнти: ананас");
             }
-            if (additional == 4)
+            else if (additional == "4")
             {
                 Console.WriteLine("Додатков1 1нгред1єнти: креветки");
             }
-            if (additional > 4)
+            else
             {
-                Console.WriteLine("Додатков1 1нгред1єнти: не вибран1");
-            }
-            if (additional < 1)
-            {
-                Console.WriteLine("Додатков1 1нгред1єнти: не вибран1");
+                Console.WriteLine("Додатков1 1нгред1єнти: не вибран1, повтор1ть спробу");
             }
         }
     }
